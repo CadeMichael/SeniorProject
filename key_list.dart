b88727760e4b84a08d2b5ca0d7b3f6ed553c0fac
Cade Michael <cade.lueker@gmail.com>
2022-02-22 18:59:05 -0500

    basic items

--- key_list.dart
+++ key_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:qr_flutter/qr_flutter.dart';
 
 /// @KeyList is a modified listview to hold saved public keys.
 ///
@@ -6,44 +8,48 @@ import 'package:flutter/material.dart';
 /// when searching for keys or adding/removing keys
 class KeyList extends StatefulWidget {
   const KeyList({Key? key}) : super(key: key);
+
   @override
   State<KeyList> createState() => _KeyListState();
 }
 
 class _KeyListState extends State<KeyList> {
   List<String> sampleKeys = ["cade's key", "tom's key"];
-  void addkey() {
+  static const IconData key = IconData(0xf052b, fontFamily: 'MaterialIcons');
+
+  void addKey(str) {
     setState(() {
-      sampleKeys.add("value");
+      sampleKeys.add(str);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Container(
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.topRight,
-          child: FloatingActionButton(
-            onPressed: addkey,
-            child: const Icon(Icons.add),
-          ),
-        ),
+
         Expanded(
           child: ListView.builder(
               padding: const EdgeInsets.all(8),
               itemCount: sampleKeys.length,
               itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-                  child: Row(
-                    children: [
-                      Text(' ${sampleKeys[index]}'),
-                    ],
-                  ),
-                );
+                return CheckboxListTile(
+                        title: Text(' ${sampleKeys[index]}'),
+                        value: true, // need to add functionality
+                        onChanged: null, // need to add functionlity
+                          secondary: const FaIcon(FontAwesomeIcons.key),
+                      );
               }),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.bottomCenter,
+          child: FloatingActionButton(
+            onPressed: () {
+              addKey("fuck");
+            },
+            child: const Icon(Icons.add),
+          ),
         ),
       ],
     );
