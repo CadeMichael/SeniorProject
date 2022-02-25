6a2ac83a62c33d9cc54551008d93b1fcc61bc3ed
Cade Michael <cade.lueker@gmail.com>
2022-02-25 16:34:48 -0500

    Expanded panel, need dialog & db integration

--- key_list.dart
+++ key_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:the_crypt/main.dart';
 //import 'package:qr_flutter/qr_flutter.dart';
 
 /// @KeyList is a modified listview to hold saved public keys.
@@ -13,40 +14,76 @@ class KeyList extends StatefulWidget {
   State<KeyList> createState() => _KeyListState();
 }
 
-class _KeyListState extends State<KeyList> {
-  List<String> sampleKeys = ["cade's key", "tom's key"];
-  static const IconData key = IconData(0xf052b, fontFamily: 'MaterialIcons');
+class ContactKey {
+  ContactKey({
+    required this.contactName,
+    required this.publicKey,
+    this.isExp = false,
+  });
 
-  void addKey(str) {
+  String contactName;
+  String publicKey;
+  bool isExp;
+}
+
+class _KeyListState extends State<KeyList> {
+  final List<ContactKey> _keys = [ContactKey(contactName: "cade", publicKey: "123")];
+
+  void addKey(ContactKey newContact) {
     setState(() {
-      sampleKeys.add(str);
+      _keys.add(newContact);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Stack(               // stack allows add button to float over SCSV
       children: [
-
-        Expanded(
-          child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: sampleKeys.length,
-              itemBuilder: (BuildContext context, int index) {
-                return CheckboxListTile(
-                        title: Text(' ${sampleKeys[index]}'),
-                        value: true, // need to add functionality
-                        onChanged: null, // need to add functionlity
-                          secondary: const FaIcon(FontAwesomeIcons.key),
-                      );
-              }),
+        SingleChildScrollView(  // SCSV allows for scrolling and gives size to ExpansionPanel
+          padding: const EdgeInsets.all(16),
+          child: ExpansionPanelList(
+            expansionCallback: (int index, bool isExp) {
+              setState(() {
+                _keys[index].isExp = !isExp;
+              });
+            },
+            // map the keys to the ExpansionPanel
+            children: _keys.map<ExpansionPanel>((ContactKey keyItem) {
+              return ExpansionPanel(
+                backgroundColor: keyItem.isExp ? darkBlue : darkBG, //if expanded change color
+                // contracted state
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    leading: const FaIcon(FontAwesomeIcons.key), //key icon
+                    title: Text(keyItem.contactName),
+                  );
+                },
+                // expaded state
+                body: ListTile(
+                    title: Text(keyItem.publicKey),
+                    subtitle: const Text(
+                        'detete Contact'),
+                    trailing: const Icon(Icons.delete),
+                    // delete function **will need to be changed to interact with db**
+                    onTap: () {
+                      setState(() {
+                        _keys.removeWhere(
+                            (ContactKey currentItem) => keyItem == currentItem);
+                      });
+                    }),
+                isExpanded: keyItem.isExp,
+              );
+            }).toList(),
+          ),
         ),
+        // add button
         Container(
           padding: const EdgeInsets.all(8),
           alignment: Alignment.bottomCenter,
           child: FloatingActionButton(
             onPressed: () {
-              addKey("fuck");
+              // need to create a dialog that pops up allowing a contact to be added
+              addKey(ContactKey(contactName: "test", publicKey: "12Test"));
             },
             child: const Icon(Icons.add),
           ),
