4a5ffb497116e0e47724bb34e6117ef744275b60
Cade Michael <cade.lueker@gmail.com>
2022-02-25 17:16:24 -0500

    comments

--- key_list.dart
+++ key_list.dart
@@ -14,13 +14,19 @@ class KeyList extends StatefulWidget {
   State<KeyList> createState() => _KeyListState();
 }
 
+/// ContactKey holds the public Keys of your contacts
+/// [contactName] is the contacts name 
+/// [publicKey] is their public key 
 class ContactKey {
+
+  // constructor 
   ContactKey({
     required this.contactName,
     required this.publicKey,
     this.isExp = false,
   });
 
+  // fields 
   String contactName;
   String publicKey;
   bool isExp;
--- personal_key.dart
+++ personal_key.dart
@@ -11,6 +11,30 @@ class PersonalKey extends StatefulWidget {
   State<PersonalKey> createState() => _PersonalKeyState();
 }
 
+/// KeyPair holds the users personal key pairs 
+/// [keyName] is name of the individual pair ie, personal or work
+/// [publicKey] is the public key 
+/// [privateKey] is the public key 
+/// [password] is the password for the private key
+class KeyPair {
+
+  // constructor 
+  KeyPair ({
+    required this.keyName,
+    required this.publicKey,
+    required this.privateKey,
+    required this.password,
+    this.isExp = false,
+  });
+
+  // fields 
+  String keyName;
+  String publicKey;
+  String privateKey;
+  String password;
+  bool isExp;
+}
+
 class _PersonalKeyState extends State<PersonalKey> {
   @override
   Widget build(BuildContext context) {
