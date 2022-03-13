4e26dea2a8ab24a0216a9182362bdf4ca7c3503e
Cade Michael <cade.lueker@gmail.com>
2022-03-13 01:34:46 -0800

    smarter UI

--- crypt.dart
+++ crypt.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:the_crypt/main.dart';
 
 /// @Crypt the widget tree that handles both encryption and
-/// decryption. 
+/// decryption.
 ///
-/// It will need a text box to input text and a search bar to 
+/// It will need a text box to input text and a search bar to
 /// determine which public key to use.
 class Crypt extends StatefulWidget {
   const Crypt({Key? key}) : super(key: key);
@@ -12,10 +13,103 @@ class Crypt extends StatefulWidget {
 }
 
 class _CryptState extends State<Crypt> {
+  // state determiners
+  bool decrypt = false;
+  bool encrypt = true;
+
+  // text controllers
+  final eCon = TextEditingController();
+  final dCon = TextEditingController();
+
+  // resulting text
+  String resultText = '';
+
+  void toggle(int i) {}
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      "where cryptology happens",
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(
+        children: [
+          Text(
+            'Encrypt',
+            style: TextStyle(color: encrypt ? lightPurp : darkBlue),
+          ),
+          Switch(
+            value: encrypt,
+            onChanged: (value) {
+              setState(() {
+                encrypt = value;
+                decrypt = !value;
+                dCon.clear();
+                eCon.clear();
+              });
+            },
+            activeColor: lightPurp,
+            inactiveThumbColor: darkBlue,
+            inactiveTrackColor: darkBlue,
+          ),
+          Text(
+            'Decrypt',
+            style: TextStyle(color: decrypt ? lightPurp : darkBlue),
+          ),
+          Switch(
+            value: decrypt,
+            onChanged: (value) {
+              setState(() {
+                decrypt = value;
+                encrypt = !value;
+                dCon.clear();
+                eCon.clear();
+              });
+            },
+            activeColor: lightPurp,
+            inactiveThumbColor: darkBlue,
+            inactiveTrackColor: darkBlue,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 48.0, 16.0, 16.0),
+            child: TextField(
+              // controller depends on toggled
+              controller: encrypt ? eCon : dCon,
+              decoration: const InputDecoration(
+                hintText: "Secret message...",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(64.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: lightPurp,
+              ),
+              padding: resultText != ''
+                  ? const EdgeInsets.all(16.0)
+                  : const EdgeInsets.all(0),
+                  child: Text(resultText, style: const TextStyle(color: darkBlue),),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: const Icon(
+                Icons.sync,
+                color: cyan,
+                size: 44,
+              ),
+              splashColor: lightPurp,
+              onPressed: () {
+                setState(() {
+                  resultText = encrypt
+                      ? eCon.text + ' but encrypted'
+                      : dCon.text + ' but decrypted';
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
