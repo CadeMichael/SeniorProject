afac2919a5c5ed7de52d22279612b7dee3ab4c11
Cade Michael <cade.lueker@gmail.com>
2022-03-13 01:42:27 -0800

    bug fix with single child scrollview

--- crypt.dart
+++ crypt.dart
@@ -29,86 +29,88 @@ class _CryptState extends State<Crypt> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32.0),
-      child: Column(
-        children: [
-          Text(
-            'Encrypt',
-            style: TextStyle(color: encrypt ? lightPurp : darkBlue),
-          ),
-          Switch(
-            value: encrypt,
-            onChanged: (value) {
-              setState(() {
-                encrypt = value;
-                decrypt = !value;
-                dCon.clear();
-                eCon.clear();
-              });
-            },
-            activeColor: lightPurp,
-            inactiveThumbColor: darkBlue,
-            inactiveTrackColor: darkBlue,
-          ),
-          Text(
-            'Decrypt',
-            style: TextStyle(color: decrypt ? lightPurp : darkBlue),
-          ),
-          Switch(
-            value: decrypt,
-            onChanged: (value) {
-              setState(() {
-                decrypt = value;
-                encrypt = !value;
-                dCon.clear();
-                eCon.clear();
-              });
-            },
-            activeColor: lightPurp,
-            inactiveThumbColor: darkBlue,
-            inactiveTrackColor: darkBlue,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 48.0, 16.0, 16.0),
-            child: TextField(
-              // controller depends on toggled
-              controller: encrypt ? eCon : dCon,
-              decoration: const InputDecoration(
-                hintText: "Secret message...",
-              ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              'Encrypt',
+              style: TextStyle(color: encrypt ? lightPurp : darkBlue),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(64.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: lightPurp,
-              ),
-              padding: resultText != ''
-                  ? const EdgeInsets.all(16.0)
-                  : const EdgeInsets.all(0),
-                  child: Text(resultText, style: const TextStyle(color: darkBlue),),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              icon: const Icon(
-                Icons.sync,
-                color: cyan,
-                size: 44,
-              ),
-              splashColor: lightPurp,
-              onPressed: () {
+            Switch(
+              value: encrypt,
+              onChanged: (value) {
                 setState(() {
-                  resultText = encrypt
-                      ? eCon.text + ' but encrypted'
-                      : dCon.text + ' but decrypted';
+                  encrypt = value;
+                  decrypt = !value;
+                  dCon.clear();
+                  eCon.clear();
                 });
               },
+              activeColor: lightPurp,
+              inactiveThumbColor: darkBlue,
+              inactiveTrackColor: darkBlue,
             ),
-          ),
-        ],
+            Text(
+              'Decrypt',
+              style: TextStyle(color: decrypt ? lightPurp : darkBlue),
+            ),
+            Switch(
+              value: decrypt,
+              onChanged: (value) {
+                setState(() {
+                  decrypt = value;
+                  encrypt = !value;
+                  dCon.clear();
+                  eCon.clear();
+                });
+              },
+              activeColor: lightPurp,
+              inactiveThumbColor: darkBlue,
+              inactiveTrackColor: darkBlue,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 48.0, 16.0, 16.0),
+              child: TextField(
+                // controller depends on toggled
+                controller: encrypt ? eCon : dCon,
+                decoration: const InputDecoration(
+                  hintText: "Secret message...",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(64.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: lightPurp,
+                ),
+                padding: resultText != ''
+                    ? const EdgeInsets.all(16.0)
+                    : const EdgeInsets.all(0),
+                    child: Text(resultText, style: const TextStyle(color: darkBlue),),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                icon: const Icon(
+                  Icons.sync,
+                  color: cyan,
+                  size: 44,
+                ),
+                splashColor: lightPurp,
+                onPressed: () {
+                  setState(() {
+                    resultText = encrypt
+                        ? eCon.text + ' but encrypted'
+                        : dCon.text + ' but decrypted';
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
